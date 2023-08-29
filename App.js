d805b393f9a487416d326589309c6a6d464da94b
DEMAND <cyril.demand@worldline.com>
2023-08-29 11:18:58 +0200

    dockerfile et header

--- App.js
+++ App.js
@@ -5,13 +5,19 @@ import ObjectDetail from './pages/ObjectDetail';
 import Order from './pages/Order';
 import Panier from './pages/Panier';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LogIn from './pages/LogIn';
+import SignIn from './pages/SignIn';
+import Header from './components/Header';
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-        <Stack.Navigator>
+        <Stack.Navigator
+        screenOptions={{ headerStyle: { backgroundColor: '#FDF7EF' } }}>
+            <Stack.Screen name="LogIn" component={LogIn} options={{ headerTitle: (props) => <Header {...props} /> }} />
+            <Stack.Screen name="SignIn"component={SignIn}/>
             <Stack.Screen name="Carte"component={Carte}/>
             <Stack.Screen name="ObjectDetail" component={ObjectDetail} />
             <Stack.Screen name="Order" component={Order} />
---
+++ pages/LogIn.js
@@ -0,0 +1,31 @@
+import React, {useState} from 'react';
+import {Text, StyleSheet, View, TextInput, Button} from 'react-native';
+
+export default function LogIn({navigation}){
+    
+    return (
+        <View >
+        <Text > Sign in </Text>
+            <View>
+                <TextInput 
+                placeholder="Email" />
+                <TextInput
+                secureTextEntry={true}
+                placeholder="Password"
+                />
+            </View>
+            <Button 
+            title="sign in"
+            onPress={() =>
+                navigation.navigate('Carte')
+            }
+            ></Button>
+            <Text 
+            onPress={() =>
+                navigation.navigate('SignIn')
+            }
+            >I don't have an account already</Text>
+            <Text>password forgotten ?</Text>
+        </View>
+    );
+};
