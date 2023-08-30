6431175e02bb66dba089371443ad74055e21fce9
DEMAND <cyril.demand@worldline.com>
2023-08-30 15:13:58 +0200

    objectDetail ok

--- pages/ObjectDetail.js
+++ pages/ObjectDetail.js
@@ -2,31 +2,82 @@ import { StatusBar } from 'expo-status-bar';
 import { Button, StyleSheet, Text, View } from 'react-native';
 import Product from '../components/Product';
 import { Image } from 'react-native-web';
+import { CheckBox } from '@rneui/base';
 
 const styles = StyleSheet.create({
     container: {
-        paddingTop: 50,
+        backgroundColor: 'white',
     },
     fullwidthImage: {
-        width: '100%', // Utilisez '100%' pour occuper toute la largeur de l'écran
+        width: '100%',
         aspectRatio: 1,
+    },
+    textContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginBottom: 10,
+        justifyContent: 'center'
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        marginRight: 10,
+    },
+    price: {
+        fontSize: 18,
+        color: 'green',
+    },
+    description: {
+        fontSize: 16,
+        marginBottom: 10,
+    },
+    allergenes: {
+        fontSize: 16,
+    },
+    divider: {
+        width: 1,
+        height: '100%',
+        marginHorizontal: 10,
+    },
+    columnContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+    halfContainer: {
+        flex: 1,
     },
 });
 
 export default function ObjectDetail({ route, navigation }) {
-    const { title, image, description, allergenes } = route.params;
-    
+    const { title, image, description, allergenes, price, check=false } = route.params;
+
     return (
-        <View>
-            <Image
+        <View style={styles.container}>
+            <View style={styles.imageContainer}>
+                <Image
                 style={styles.fullwidthImage}
                 source={{
                     uri: image,
                 }}
-            />
-            <Text>{title}</Text>
-            <Text>{description}</Text>
-            <Text>{allergenes}</Text>
+                />
+            </View>
+            <View style={styles.textContainer}>
+                <Text style={styles.title}>{title}</Text>
+                <Text style={styles.price}>{price} €</Text>
+                <CheckBox checked={check}></CheckBox>
+            </View>
+
+            <View style={styles.columnContainer}>
+                <View style={styles.halfContainer}>
+                    <Text style={styles.title}>Description</Text>
+                    <Text style={styles.description}>{description}</Text>
+                </View>
+                <View style={styles.divider}></View>
+                <View style={styles.halfContainer}>
+                    <Text style={styles.title}>Allergènes</Text>
+                    <Text style={styles.allergenes}>{allergenes}</Text>
+                </View>
+            </View>
         </View>
     );
 }
