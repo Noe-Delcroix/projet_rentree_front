f132a6ee5e65b20c189ab2295ab6723d53d75610
Cyrildemand <cydemand@gmail.com>
2023-09-05 16:38:49 +0200

    alexis

--- pages/Order.js
+++ pages/Order.js
@@ -1,29 +1,48 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { View, Text, StyleSheet, Image } from 'react-native';
+import axios from "axios";
+import {useApplicationContext} from "../components/ApplicationContext";
 
 export default function Order({ route, navigation }) {
-    //const { price } = route.params;
+    const [solde, setSolde] = useState(0);
 
-    const soldeRestant = () => {
-        // TODO: Faire appel à l'API avec Axios pour calculer le solde restant
-        return 15.50
+    const { token } = useApplicationContext();
+
+    const soldeRestant = async () => {
+        try{
+            const response = await axios.get('http://localhost:8080/api/users/info', {
+                headers: {
+                    token: token,
+                },
+            });
+            console.log(response.data);
+            setSolde(response.data.balance);
+        } catch (error) {
+            console.log('ERREUR');
+            console.error(error);
+        }
+
     };
+
+    useEffect(() =>{
+        soldeRestant().then(r => console.log(r));
+    },[]);
 
     return (
         <View style={styles.container}>
-        <Image
-            style={styles.image}
-            source={{
-            uri: 'https://i.guim.co.uk/img/media/88f6b98714035656cb18fb282507b60e82edb0d7/0_57_2560_1536/master/2560.jpg?width=1200&height=900&quality=85&auto=format&fit=crop&s=74b1dd992e5428e4906b1e331a57a305',
-            }}
-        />
-        <Text style={styles.title}>Commande validée !</Text>
-        <Text style={styles.subtitle}>
-            Elle vous attendra à la fin de vos rattrapages !
-        </Text>
-        <Text style={styles.solde}>
-            Solde CROUS restant : {soldeRestant()} €
-        </Text>
+            <Image
+                style={styles.image}
+                source={{
+                    uri: 'https://i.guim.co.uk/img/media/88f6b98714035656cb18fb282507b60e82edb0d7/0_57_2560_1536/master/2560.jpg?width=1200&height=900&quality=85&auto=format&fit=crop&s=74b1dd992e5428e4906b1e331a57a305',
+                }}
+            />
+            <Text style={styles.title}>Commande validée !</Text>
+            <Text style={styles.subtitle}>
+                Elle vous attendra à la fin de vos rattrapages !
+            </Text>
+            <Text style={styles.solde}>
+                Solde CROUS restant : {solde} €
+            </Text>
         </View>
     );
 }
@@ -47,11 +66,9 @@ const styles = StyleSheet.create({
     },
     subtitle: {
         fontSize: 18,
-        textAlign: 'center', // Alignement du texte au centre
-        marginBottom: 20,
-    },
-    solde: {
-        fontSize: 20,
-        color: 'green', // Couleur du solde
-  },
-});
+        textAlign: 'center', // Alignement du texte au
+        solde: {
+            fontSize: 20,
+            color: 'green', // Couleur du solde
+        },
+    }});
