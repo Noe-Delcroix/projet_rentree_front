a86d56d272c7afa30cbcc24ec14bb3d289fd0e5b
Noe-Delcroix <noe.delcroix59@gmail.com>
2023-09-23 16:36:49 +0200

    css login page

--- src/pages/LogIn.js
+++ src/pages/LogIn.js
@@ -69,28 +69,41 @@ export default function LogIn({ navigation }) {
     }
 
     return (
-        <View>
-        <Text className="text-6xl font-bold">{login ? 'Log In' : 'Sign In'}</Text>
-        <View>
-            {login ? <></> : <TextInput placeholder="firstname" onChangeText={setFirstname}/>}
-            {login ? <></> : <TextInput placeholder="lastname" onChangeText={setLastname}/>}
-            <TextInput placeholder="Email" onChangeText={setEmail}/>
-            <TextInput
-                secureTextEntry={true}
-                placeholder="Password"
-                onChangeText={setPassword}
-            />
-            <Button
-            title={login ? 'Log In' : 'Sign In'}
-            onPress={login ? () => tryLogIn() : () => trySignIn()}
-            />
-            <Text onPress={() => setLogin(!login)}>
-            {login ? "I don't have an account already" : 'I already have an account'}
-            </Text>
-            <Text onPress={sendPasswordResetEmail}>
-            {login ? 'Password forgotten ?' : ''}
-            </Text>
-        </View>
+        <View className="flex-1 justify-center items-center">
+            <View className="w-full md:w-1/3 p-10 flex flex-col items-center justify-center bg-[#FDF7EF] shadow-xl">
+                <Text className="text-5xl font-bold mb-10">{login ? 'Log In' : 'Sign Up'}</Text>
+                <View className="w-full">
+                    {login ? null : <TextInput className="mb-2 p-2 border border-[#713235] rounded" placeholder="First Name" onChangeText={setFirstname}/>}
+                    {login ? null : <TextInput className="mb-2 p-2 border border-[#713235] rounded" placeholder="Last Name" onChangeText={setLastname}/>}
+                    <TextInput className="mb-2 p-2 border border-[#713235] rounded" placeholder="Email" onChangeText={setEmail}/>
+                    <TextInput
+                        className="mb-2 p-2 border border-[#713235] rounded"
+                        secureTextEntry={true}
+                        placeholder="Password"
+                        onChangeText={setPassword}
+                    />
+                    <View className="my-5">
+                        <Button
+                            color={'#713235'}
+                            className="text-white p-2 rounded"
+                            title={login ? 'Log In' : 'Sign Up'}
+                            onPress={login ? () => tryLogIn() : () => trySignUp()}
+                        />
+                    </View>
+
+                    <Text className="mb-2 text-[#713235] text-center" onPress={() => setLogin(!login)}>
+                        {login ? "Create an account" : 'I already have an account'}
+                    </Text>
+                    {login && (
+                        <Text className="text-[#713235] text-center" onPress={sendPasswordResetEmail}>
+                            Forgot your password?
+                        </Text>
+                    )}
+                </View>
+            </View>
+
+
+
         </View>
     );
 }
