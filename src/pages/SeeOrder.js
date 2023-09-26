4b8c624ef92de6f8d9862b036fedf59624da1980
Noe-Delcroix <noe.delcroix59@gmail.com>
2023-09-26 14:24:20 +0200

    fin css histo commande + fex fixes

--- src/pages/SeeOrder.js
+++ src/pages/SeeOrder.js
@@ -6,24 +6,26 @@ import {useDispatch, useSelector} from "react-redux";
 import {loadDetailledOrders, loadOrders} from "../slices/Orders";
 import BottomNavigationBar from "../components/BottomNavigationBar";
 import SortingForm from "../components/SortingForm";
+import {loadDetailledBasket} from "../slices/Basket";
 
 export default function SeeOrder({ navigation }) {
     const {IsAnyUserLogedIn} = useApplicationContext();
 
     const dispatch = useDispatch();
     const [sortType, setSortType] = useState("DATE");
-    const [sortingOrders, setSortingOrders] = useState("ASC");
+    const [sortingOrders, setSortingOrders] = useState('asc')
 
     const orders = useSelector(state => state.orders.value)
 
-    useEffect(() => {
-        // if (!IsAnyUserLogedIn()) {
-        //     navigation.replace('Carte');
-        // }else{
-        //
-        // }
-        dispatch(loadOrders({ sortType, sortingOrders }))
-    }, []);
+
+    React.useCallback(() => {
+        if (!IsAnyUserLogedIn()) {
+            navigation.replace('Carte');
+        }else{
+            dispatch(loadOrders({ sortType, sortingOrders }))
+        }
+        return () => {};
+    }, [dispatch])
 
     const handleSortingChange = ({ sortType, sortOrder }) => {
         setSortType(sortType);
@@ -35,10 +37,9 @@ export default function SeeOrder({ navigation }) {
         <View className="flex-1">
             <ScrollView>
                 <View className="mx-5 xl:mx-48">
-
-                    <View className="bg-white mt-10 mb-5 p-5 shadow-xl">
+                    <View className="bg-white mt-10 p-5 shadow-xl">
                         <SortingForm
-                            sortingMethods={{PRICE: "Prix",DATE: "date"}}
+                            sortingMethods={{PRICE: "Prix",DATE: "Date"}}
                             sortType={sortType}
                             sortOrder={sortingOrders}
                             onSortingChange={handleSortingChange}
@@ -46,7 +47,7 @@ export default function SeeOrder({ navigation }) {
                     </View>
 
                     <View className="mt-10 mb-5 p-5 w-full">
-                        <Text className="text-2xl ">Vos commandes</Text>
+                        <Text className="text-4xl">Vos commandes</Text>
                         <View className="w-full sm:w-1/2 h-1 bg-[#713235] mb-3"></View>
 
 
